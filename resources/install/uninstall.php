f37e8c1dc218c7827d153ee3fe0fa36847b35cd0
Israel D. Canasa <rael@timble.net>
2015-04-07 23:50:00 +0800

    Re #20: On Uninstall, execute uninstall.sql

--- resources/install/uninstall.php
+++ resources/install/uninstall.php
@@ -1,5 +1,17 @@
 <?php
 
 function todo_uninstall() {
+    $db  = $GLOBALS['wpdb'];
+    $sql = str_replace('#__', $db->prefix, file_get_contents(__DIR__.'/uninstall.sql'));
 
+    $matches = array();
+    preg_match_all("/^DROP(?:[^;]|(?:'.*?'))+;\\n*$/im", $sql, $matches);
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+    foreach($matches[0] as $sql) {
+        $wpdb->query($sql);
+    }
+
+    delete_option('todo_installed');
 }
