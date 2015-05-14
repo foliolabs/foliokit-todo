6889436545fc43c41a5da91007ac7afb102188f3
Israel D. Canasa <rael@timble.net>
2015-05-15 00:28:56 +0800

    Re #20: Made the router for Todo
    This will be called by the routing system for Nooku for WordPress.

---
+++ code/plugins/nooku-todo/site/dispatcher/router.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Nooku Framework for Wordpress - http://nooku.org/framework
+ *
+ * @copyright   Copyright (C) 2007 - 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/nooku/nooku-framework-wordpress for the canonical source repository
+ */
+
+/**
+ *  Todo Dispatcher Router
+ *
+ * @author  Johan Janssens <https://github.com/johanjanssens>
+ * @package Koowa\Component\Koowa\Dispatcher\Request
+ */
+class ComTodoDispatcherRouter extends ComKoowaDispatcherRouterAbstract
+{
+    public function parse(KHttpUrlInterface $url)
+    {
+        $segments = $url->getPath(true);
+        $query    = array();
+
+        if(count($segments))
+        {
+            $view = array_shift($segments);
+
+            if(in_array($view, array('item', 'items')))
+            {
+                $query['view'] = $view;
+
+                if($id = array_shift($segments) && $view == 'item') {
+                    $query['id'] = $id;
+                }
+            }
+        }
+
+        return $query;
+    }
+
+    public function build(KHttpUrlInterface $url)
+    {
+        $query    = $url->getQuery(true);
+        $segments = array();
+
+        if(isset($query['view']))
+        {
+            $segments[] = $query['view'];
+            unset( $query['view'] );
+        }
+
+        if(isset($query['id']))
+        {
+            $segments[] = $query['id'];
+            unset( $query['id'] );
+        }
+
+        $url->setQuery($query);
+        $url->setPath(implode('/', $segments));
+
+        return parent::build($url);
+    }
+}
